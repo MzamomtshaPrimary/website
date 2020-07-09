bf5f219cb596c9653ceafe57b879a5d5f3846d4f
MzamomtshaPrimary <66827282+MzamomtshaPrimary@users.noreply.github.com>
2020-07-09 14:00:06 +0200

    Add files via upload

---
+++ iframe.js
@@ -0,0 +1,17 @@
+/*var div = document.querySelector('.parent-iframe');
+document.addEventListener('mousemove', function(e){
+  div.style.transform = 'translateY('+(e.clientY-12)+'px)';
+  div.style.transform += 'translateX('+(e.clientX-19)+'px)';
+  //console.log(`x: ${e.x} | y: ${e.y}`);
+});*/
+function Open(){
+window.open("https://www.youtube.com/watch?v=Y1drw8jQGrw");
+}
+
+//window.onload = document.body.appendChild(document.createElement('iframe').src="https://www.youtube.com/embed/Y1drw8jQGrw";);
+
+
+//<iframe width="1349" height="480"
+//src="https://www.youtube.com/embed/Y1drw8jQGrw" frameborder="0"
+//allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+//allowfullscreen></iframe>
